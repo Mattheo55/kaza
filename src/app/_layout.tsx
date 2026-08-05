cbc6f4cd0d932500c32ad1b03b966f2e37728d6a
Matthéo <mattheo55.b@gmail.com>
2026-08-05 21:08:47 +0200

    5 - snpro and papicons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,13 @@
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
 import "./global.css";
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "snpro-medium": require('../../fonts/SNPro-Medium.ttf'),
+    "snpro-bold": require('../../fonts/SNPro-Bold.ttf'),
+  })
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index"/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sn": "snpro-medium",
+        "sn-bold": "snpro-bold"
+      }
+    },
   },
   plugins: [],
 }
