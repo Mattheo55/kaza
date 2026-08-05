eb465705f2b6ebf0da1eb70b39918840fff07d4e
Matthéo <mattheo55.b@gmail.com>
2026-08-05 21:30:50 +0200

    fix

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,26 @@
 import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import "./global.css";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     "snpro-medium": require('../../fonts/SNPro-Medium.ttf'),
     "snpro-bold": require('../../fonts/SNPro-Bold.ttf'),
-  })
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
