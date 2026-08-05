735ccaf3445db5f3d8a99368efd1fd49a66ae7ae
Matthéo <31368972+Mattheo55@users.noreply.github.com>
2026-08-05 21:20:15 +0200

    Update src/app/index.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>Edit src/app/index.tsx to edit this screen .</Text>
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </SafeAreaView>
   );
 }
